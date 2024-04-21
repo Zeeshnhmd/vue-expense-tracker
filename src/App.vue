38338ca8e6145ac0cbaa0b36bcfed065efad8de3
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2024-04-21 17:07:03 +0530

    feat: total for balance, income, expense

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
 	<Header />
 	<div class="container">
-		<Balance />
-		<IncomeExpenses />
+		<Balance :total="total" />
+		<IncomeExpenses :income="income" :expense="expense" />
 		<TransactionList :transactions="transactions" />
 		<AddTransaction />
 	</div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
@@ -23,4 +23,31 @@ const transactions = ref([
 	{ id: 3, text: 'Book', amount: -10 },
 	{ id: 4, text: 'Camera', amount: 150 },
 ]);
+
+// Get Total
+const total = computed(() => {
+	return transactions.value.reduce((acc, transaction) => {
+		return acc + transaction.amount;
+	}, 0);
+});
+
+// Get Income
+const income = computed(() => {
+	return transactions.value
+		.filter((el) => el.amount > 0)
+		.reduce((acc, transaction) => {
+			return acc + transaction.amount;
+		}, 0)
+		.toFixed(2);
+});
+
+// Get expenses
+const expense = computed(() => {
+	return transactions.value
+		.filter((el) => el.amount < 0)
+		.reduce((acc, transaction) => {
+			return acc + transaction.amount;
+		}, 0)
+		.toFixed(2);
+});
 </script>
